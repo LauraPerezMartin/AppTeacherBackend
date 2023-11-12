3cd27f5adb2b5b6605fe32695348acfaa1a50cf0
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-11-12 19:47:01 +0100

    Creamos la ruta users y la añadimos a la api

--- routes/api.js
+++ routes/api.js
@@ -2,4 +2,6 @@ const router = require('express').Router();
 
 router.use('/public', require('./api/public'));
 
+router.use('/users', require('./api/users'))
+
 module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+
+router.post('/login', (req, res) => {
+
+});
+
+router.post('/registro', (req, res) => {
+
+})
+
+module.exports = router;

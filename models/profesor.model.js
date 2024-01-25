575079b1472a80b6ba9a0ac2b6b613e4538d9d6d
laurap48@hotmail.com <laurap48@hotmail.com>
2024-01-25 19:25:48 +0100

    Creamos en el modelo profesores updateValidacion, cambios en la funcion getProfesorById (renombrada como getProfesorByUsuarioId) y al modelo de usuario añadimos deleteById

--- models/profesor.model.js
+++ models/profesor.model.js
@@ -2,19 +2,24 @@ const getProfesoresPublic = () => {
     return db.query('SELECT u.id, u.nombre, u.apellidos, u.direccion, u.ciudad, u.latitud, u.longitud, u.imagen, p.experiencia, p.precio FROM usuarios AS u, profesores AS p WHERE u.rol="profe" AND u.id=p.usuario_id AND p.validado=1 AND u.borrado=0');
 }
 
-const getAllProfesores = () => {
-    return db.query('SELECT u.*, p.experiencia, p.precio FROM usuarios AS u, profesores AS p WHERE u.rol="profe" AND u.id = p.usuario_id AND u.borrado=0');
-}
-
 const getCiudadesConProfesores = () => {
     return db.query('SELECT DISTINCT ciudad FROM usuarios WHERE rol="profe" AND p.validado=1 AND u.borrado=0');
 }
 
-const getProfesorById = (profesorId) => {
-    return db.query('SELECT * FROM profesores WHERE id=?', [profesorId]);
+const getAllProfesores = () => {
+    return db.query('SELECT u.*, p.experiencia, p.precio FROM usuarios AS u, profesores AS p WHERE u.rol="profe" AND u.id = p.usuario_id AND u.borrado=0');
+}
+
+const getProfesorByUsuarioId = (usuarioId) => {
+    return db.query('SELECT u.*, p.experiencia, p.precio, p.validado FROM usuarios AS u, profesores AS p WHERE u.id=? and u.rol="profe" AND u.id= p.usuario_id AND u.borrado=0', [usuarioId]);
+}
+
+const updateValidacion = (usuarioId, validacion) => {
+    return db.query('UPDATE profesores SET validado=? WHERE usuario_id=?', [validacion, usuarioId]);
 }
 
 const create = (usuarioId, { experiencia, precio }) => {
     return db.query('INSERT INTO profesores (experiencia, precio, usuario_id)  VALUES (?, ?, ?)', [experiencia, precio, usuarioId]);
 }
-module.exports = { getProfesoresPublic, getAllProfesores, getCiudadesConProfesores, getProfesorById, create };
+
+module.exports = { getProfesoresPublic, getAllProfesores, getCiudadesConProfesores, getProfesorByUsuarioId, updateValidacion, create };
--- models/usuario.model.js
+++ models/usuario.model.js
@@ -6,10 +6,18 @@ const getUsuarioByMail = (email) => {
     return db.query('SELECT * FROM usuarios WHERE email=?', [email]);
 };
 
+const getUsuariosByRol = (rol) => {
+    return db.query('SELECT * FROM ususario WHERE rol=?', [rol]);
+}
+
 const create = ({ nombre, apellidos, username, email, password, telefono, direccion, ciudad, latitud, longitud, imagen, edad, fecha_nacimiento, genero, dni, rol }) => {
     return db.query(
         'INSERT INTO usuarios (nombre, apellidos, username, email, password, telefono, direccion, ciudad, latitud, longitud, imagen, edad, fecha_nacimiento, genero, dni, rol) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
         [nombre, apellidos, username, email, password, telefono, direccion, ciudad, latitud, longitud, imagen, edad, fecha_nacimiento, genero, dni, rol]);
 }
 
-module.exports = { getUsuarioById, getUsuarioByMail, create };
+const deleteById = (usuarioId) => {
+    return db.query('UPDATE usuarios SET borrado=1 WHERE id=?', [usuarioId]);
+}
+
+module.exports = { getUsuarioById, getUsuarioByMail, getUsuariosByRol, create, deleteById };
